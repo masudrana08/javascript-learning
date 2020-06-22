b465e47f05407cb0a89c7e0b1866fd0de8cdb10c
masudrana08 <rana028511@gmail.com>
2020-06-22 08:29:14 +0600

    function 7

---
+++ function7.js
@@ -0,0 +1,21 @@
+var arr=[3,4,5,6,7,8,8];
+// var sum=0;
+// for (var i=0; i<arr.length; i++){
+//     sum+=arr[i];
+// }
+// console.log(sum);
+
+
+// var a=arr.reduce(function(a,b){
+//     return a+b;
+// });
+// console.log(a);
+
+var sum=0;
+function myfunc(array){
+    for(var i=0; i<array.length; i++){
+        sum+=array[i];
+    }
+}
+myfunc(arr)
+console.log(sum);
