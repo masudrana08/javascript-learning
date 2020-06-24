766672db02d311d0b88e8f9df18d9a09f54b5e2f
masudrana08 <rana028511@gmail.com>
2020-06-24 08:31:53 +0600

    es6 1

---
+++ es61.js
@@ -0,0 +1,54 @@
+// const x=6;
+// function myfunc(){
+//     var x=5;
+//     console.log(x)
+// }
+// myfunc();
+// console.log(x)
+
+// const arr=[];
+// arr.push(5);
+// console.log(arr)
+
+// var a=20;
+// if(true){
+    
+//     let a=10;
+    
+//     console.log(a)
+// }
+// console.log(a)
+
+// const name="masud";
+// const age=18;
+// const mail="masud@gmail.com";
+
+// // const details=`
+// //     name : ${name};
+// //     age : ${age};
+// //     mail : ${mail}
+// //     amar sona : ${"jadu"}
+// // `
+// const details="name : "+name+", age : "+age+", mail : "+mail
+// console.log(details)
+
+
+let b=10;
+// function F(){
+//     {
+//         var a=20;
+//         console.log(a)
+//     }
+// }console.log(a)
+// F()
+// console.log(a)
+if(true){
+    let a=20;
+    console.log(a)
+    {  a=5;
+        console.log(a)
+    }
+    a=50;
+}
+var b=40
+console.log(a)
